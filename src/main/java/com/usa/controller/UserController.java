f5ec7727e025553cc11f759abaf54d3fb2ca21d6
JuanCMurcia <vrinda3d@gmail.com>
2021-11-20 22:53:54 -0500

    Actualizar Buenas Practicas UserController.java

--- src/main/java/com/usa/controller/UserController.java
+++ src/main/java/com/usa/controller/UserController.java
@@ -8,30 +8,56 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controlador para llamados Api
+ */
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
 public class UserController {
 
+    /**
+     * Auto incrementado
+     */
     @Autowired
     private UserService userService;
 
+    /**
+     * Metodo que lista los usuarios
+     * @return
+     */
     @GetMapping("/all")
     public List<User> getAll(){
         return userService.getAll();
     }
 
+    /**
+     * Metodo para ingresar un nuevo usuario
+     * @param user
+     * @return
+     */
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User registro(@RequestBody User user){
         return userService.registro(user);
     }
 
+    /**
+     * Metodo para validar las llaves email / password
+     * @param email
+     * @param password
+     * @return
+     */
     @GetMapping("/{email}/{password}")
     public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password){
         return userService.autenticarUsuario(email, password);
     }
 
+    /**
+     * Metodo para Evaluar la existencia del email
+     * @param email
+     * @return
+     */
     @GetMapping("/{email}")
     public boolean existeEmail(@PathVariable("email") String email){
         return userService.existeEmail(email);
